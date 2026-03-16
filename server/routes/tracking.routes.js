f513500f824535e44864758448203d3f809f6374
FANOMI <dev@fanomi.local>
2026-03-16 15:22:37 -0300

    fix: corregir endpoint MRW al URL real del WSDL clientesbalanceo2

--- server/routes/tracking.routes.js
+++ server/routes/tracking.routes.js
@@ -116,7 +116,7 @@ router.post("/mrw-sync", auth, async (req, res) => {
   </soapenv:Body>
 </soapenv:Envelope>`;
 
-        const response = await fetch("https://trackingservice.mrw.es/TrackingServices/TrackingService.svc", {
+        const response = await fetch("http://clientesbalanceo2.mrw.es/TrackingServices/TrackingService.svc", {
           method: "POST",
           headers: {
             "Content-Type": "text/xml; charset=utf-8",
